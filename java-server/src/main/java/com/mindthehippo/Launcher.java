aa4a1e4ec16e1e367aabac5b49f122c410a131b2
morgade <>
2016-10-19 00:19:42 -0300

    Added no-cache forced header filter due to IE caching rest GETs

--- java-server/src/main/java/com/mindthehippo/Launcher.java
+++ java-server/src/main/java/com/mindthehippo/Launcher.java
@@ -1,8 +1,17 @@
 package com.mindthehippo;
 
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -32,4 +41,22 @@ public class Launcher extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
     }
+    
+    /**
+     * Avoid @RestController caching problems on older IE
+     * @return 
+     */
+    @Bean
+    public Filter noChacheFilterForIE() {
+        return new GenericFilterBean() {
+            @Override
+            public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+                ((HttpServletResponse)sr1).addHeader("Pragma", "no-cache");
+                ((HttpServletResponse)sr1).addHeader("Cache-control", "no-cache");
+                fc.doFilter(sr, sr1);
+            }
+        };
+  }
+    
+
 }
